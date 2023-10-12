8eeae2ca466bcfd93e95a1a2fc87a9035827f8c2
Antoh1 <antotiro@gmail.com>
2023-10-12 17:01:26 +0300

    [UPD] create data by loop and make get request from input

--- consume.py
+++ consume.py
@@ -2,6 +2,19 @@ import requests
 
 url = 'http://127.0.0.1:5000/'
 
-response = requests.put(url + '/user/4', {'likes': 20})
+data = [{'likes': 20, 'views':43, 'name':'Anaconda Tutorial'},
+        {'likes': 67, 'views':45000, 'name':'Flask Tutorial'},
+        {'likes': 30, 'views':3400, 'name':'Django Tutorial'},]
 
-print(response.json())
+for i_d in range(len(data)):
+    response = requests.put(url + 'user/' + str(i_d), data[i_d])
+    print(response.json())
+user_id = int(input())
+response2 = requests.get(url + 'user/' + str(user_id))
+print(response2.json())
+# input()
+# response3 = requests.delete(url + 'user/4')
+# print(response2.json())
+# input()
+# response2 = requests.get(url + 'user/4')
+# print(response2.json())
