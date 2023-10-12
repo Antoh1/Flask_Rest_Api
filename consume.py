6c8bb252a7818b35fb41d8130f8b2170fef1fa39
Antoh1 <antotiro@gmail.com>
2023-10-12 19:44:03 +0300

    [UPT] Creating and updating data via api

--- consume.py
+++ consume.py
@@ -7,14 +7,18 @@ data = [{'likes': 20, 'views':43, 'name':'Anaconda Tutorial'},
         {'likes': 30, 'views':3400, 'name':'Django Tutorial'},]
 
 for i_d in range(len(data)):
-    response = requests.put(url + 'user/' + str(i_d), data[i_d])
+    response = requests.put(url + 'user/' + str(i_d+1), data[i_d])
     print(response.json())
 user_id = int(input())
 response2 = requests.get(url + 'user/' + str(user_id))
 print(response2.json())
+patcho = {'name':'Code with Anto'}
+user_id = int(input())
+response4 = requests.patch(url + 'user/' + str(user_id), patcho)
+print(response4.json())
 # input()
-# response3 = requests.delete(url + 'user/4')
-# print(response2.json())
+# response3 = requests.delete(url + 'user/2')
+# print(response3)
 # input()
-# response2 = requests.get(url + 'user/4')
+# response2 = requests.get(url + 'user/0')
 # print(response2.json())
